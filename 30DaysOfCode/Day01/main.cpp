c1c04b66264beeb42d3a82800c118a28de778f8a
Ryan Carr <rycarr@gmail.com>
2019-06-24 09:55:22 -0500

    Day01 Data Types - Solved
    This is the solution to the problem. I wanted to personalize it a
    bit more but they had certain lines of code that couldn't be changed.

--- 30DaysOfCode/Day01/main.cpp
+++ 30DaysOfCode/Day01/main.cpp
@@ -4,24 +4,30 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int i = 4;
     double d = 4.0;
     string s = "HackerRank ";
 
-    
     // Declare second integer, double, and String variables.
+    int secondInteger = NULL;
+    double secondDouble = NULL;
+    string secondString = "";
     
-    // Read and save an integer, double, and String to your variables.
-    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
     
+    cin >> secondInteger;
+    cin >> secondDouble;
+    cin.ignore(); // Ignore the newline character
+    getline(cin, secondString);
+
     // Print the sum of both integer variables on a new line.
+    cout << i + secondInteger << endl;
     
     // Print the sum of the double variables on a new line.
+    cout << fixed << setprecision(1) << d + secondDouble << endl;
     
     // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
+    cout << s << secondString << endl;
 
     return 0;
 }
