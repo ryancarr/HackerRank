4b8f84a87498f4474c1528b93562dcd629cb90d3
Ryan Carr <rycarr@gmail.com>
2019-06-26 20:02:54 -0500

    Day03 - Solved
    This solution passes all of the tests.

--- 30DaysOfCode/Day03/main.cpp
+++ 30DaysOfCode/Day03/main.cpp
@@ -10,5 +10,20 @@ int main()
     cin >> N;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
+    bool isEven = (N % 2 == 0);
+        
+    // If n is odd, print Weird
+    if(!isEven)
+        cout << "Weird" << endl;
+    // If n is even and in the inclusive range of 2 to 5, print Not Weird
+    else if(isEven && (2 <= N && N <= 5))
+        cout << "Not Weird" << endl;
+    // If n is even and in the inclusive range of 6 to 20, print Weird
+    else if(isEven && (6 <= N && N <= 20))
+        cout << "Weird" << endl;
+    // If n is even and greater than 20, print Not Weird
+    else if(isEven && N > 20)
+        cout << "Not Weird" << endl;
+
     return 0;
 }
