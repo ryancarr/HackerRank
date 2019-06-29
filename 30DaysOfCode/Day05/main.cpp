b641d34fe9c1a73f913607d1961ad2280e587c4c
Ryan Carr <rycarr@gmail.com>
2019-06-28 20:31:26 -0500

    Day 05: Loops - Solved
    Wrote the solution to the code. Having trouble submitting it on the
    website, but it has passed multiple tests of my own, so I'm calling
    it solved.

--- 30DaysOfCode/Day05/main.cpp
+++ 30DaysOfCode/Day05/main.cpp
@@ -10,5 +10,8 @@ int main()
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
+    for(int i = 1; i < 11; i++)
+        cout << n << " x " << i << " = " << n * i << endl;
+
     return 0;
 }
