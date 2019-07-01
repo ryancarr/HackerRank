0f2cabe6215b7b4f7cc4f729f62d93c1efb52f75
Ryan Carr <rycarr@gmail.com>
2019-07-01 11:06:48 -0500

    Day 08: Dictionaries and Maps - Solved
    This is my solution to the Dictionaries and Maps challenge.

--- 30DaysOfCode/Day08/main.cpp
+++ 30DaysOfCode/Day08/main.cpp
@@ -1,13 +1,65 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <map>
-using namespace std;
+#include <istream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::istringstream;
+using std::istream_iterator;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+vector<string> splitString(string);
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main()
+{
+    unordered_map<string, string> phoneBook;
+    string line;
+
+    int count;
+    cin >> count;
+    cin.ignore();
+
+    for(int i = 0; i < count; i++)
+    {
+        getline(cin, line);
+        vector<string> values = splitString(line);
+
+        phoneBook[values[0]] = values[1];
+    }
+
+    while(getline(cin, line))
+    {
+        auto position = phoneBook.find(line);
+
+        if(position == phoneBook.end())
+            cout << "Not found" << endl;
+        else
+            cout << position->first << "=" << position->second << endl;
+        
+    }
+
     return 0;
+}
+
+/**
+ * Splits a string on the white space
+ * @param line The string to be split
+ * @return A vector containing individual words in each index
+ */
+vector<string> splitString(string line)
+{
+    istringstream buff(line);
+    istream_iterator<string> begin(buff), end;
+    vector<string> values(begin, end);
+
+    return values;
 }
