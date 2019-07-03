46aaf5c3b05b0a6b43a7df0b6933c38104a5e0b7
Ryan Carr <rycarr@gmail.com>
2019-07-03 11:15:44 -0500

    Day 10: Binary Numbers - Cleaned up
    Added documentation and streamlined the code that calculated how
    many consecutive 1's are in the binary number.

--- 30DaysOfCode/Day10/main.cpp
+++ 30DaysOfCode/Day10/main.cpp
@@ -9,17 +9,24 @@ using std::stack;
 using std::string;
 using std::to_string;
 
+/**
+ * Converts a base 10 number to base 2
+ * @param number The number to be converted to base 2
+ * @return A string representing the base 2 version of the number
+ */
 string convertToBaseTwo(int number)
 {
     stack<int> stk;
     string result = "";
 
+    // Populate the stack
     while(number > 0)
     {
         stk.push(number % 2);
         number = number / 2;
     }
 
+    // Build the result string by popping off numbers
     while(!stk.empty())
     {
         result += to_string(stk.top());
@@ -32,32 +39,29 @@ string convertToBaseTwo(int number)
 int main()
 {
     int number = 0;
-    string result;
-    cin >> number;
-    cin.ignore();
+    cin >> number; // Get a number from the user
+    cin.ignore();  // Ignore trailing new line characters
 
+    string result;
     result = convertToBaseTwo(number);
 
     int consecutive = 0;
     int max = 0;
-    bool last = false;
 
+    // Loop over every character in result
     for(char c : result)
     {
-        if(c == '1')
-        {
-            last = true;
-            consecutive++;
-        }
-        else
-        {
-            last = false;
-            consecutive = 0;
-        }
+        // Increment consecutive count if character is 1
+        if(c == '1') consecutive++;
 
+        // Reset counter if character is anything other than 1
+        else consecutive = 0;
+
+        // Keep track of max value of consecutive
         if(max < consecutive) max = consecutive;
     }
 
+    // Send max to STDOUT
     cout << max << endl;
     
     return 0;
