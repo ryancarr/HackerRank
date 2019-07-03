104cdcbc492c31c65b21335e1f754b055f9f20d4
Ryan Carr <rycarr@gmail.com>
2019-07-03 11:05:08 -0500

    Day 10: Binary Numbers - Solved
    Solved the challenge and passed all of the test cases.

--- 30DaysOfCode/Day10/main.cpp
+++ 30DaysOfCode/Day10/main.cpp
@@ -1,14 +1,64 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <string>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::stack;
+using std::string;
+using std::to_string;
 
+string convertToBaseTwo(int number)
+{
+    stack<int> stk;
+    string result = "";
 
+    while(number > 0)
+    {
+        stk.push(number % 2);
+        number = number / 2;
+    }
+
+    while(!stk.empty())
+    {
+        result += to_string(stk.top());
+        stk.pop();
+    }
+
+    return result;
+}
 
 int main()
 {
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int number = 0;
+    string result;
+    cin >> number;
+    cin.ignore();
 
+    result = convertToBaseTwo(number);
+
+    int consecutive = 0;
+    int max = 0;
+    bool last = false;
+
+    for(char c : result)
+    {
+        if(c == '1')
+        {
+            last = true;
+            consecutive++;
+        }
+        else
+        {
+            last = false;
+            consecutive = 0;
+        }
+
+        if(max < consecutive) max = consecutive;
+    }
+
+    cout << max << endl;
+    
     return 0;
 }
