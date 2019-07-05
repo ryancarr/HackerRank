eda932fa8d8c41bd978e971bf201aabc2c19340b
Ryan Carr <rycarr@gmail.com>
2019-07-04 19:42:31 -0500

    Day 11: 2D Array - Solved
    The code has successfully passed all of the test cases put forth
    by the challenge.

--- 30DaysOfCode/Day11/main.cpp
+++ 30DaysOfCode/Day11/main.cpp
@@ -19,5 +19,29 @@ int main()
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
+    // Set max to minimum possible number represented by int
+    int max = std::numeric_limits<int>::min();
+
+    for(int i = 0; i < arr.size() - 2;i++)
+    {
+        int sum = 0;
+
+        for(int j = 0; j < arr[i].size() - 2;j++)
+        {
+            // Add top cross piece
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+            
+            // Add middle square
+            sum += arr[i+1][j+1];
+
+            // Add bottom cross piece
+            sum += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+            if(max < sum) max = sum;
+        }
+    }
+
+    cout << max;
+
     return 0;
 }
