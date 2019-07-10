a886a9889ed94004238e1123aae8b4c40036b421
Ryan Carr <rycarr@gmail.com>
2019-07-10 18:42:59 -0500

    Day 17: More Exceptions - Solved
    This is my solution to the exceptions challenge. This code passes
    both test conditions.

--- 30DaysOfCode/Day17/main.cpp
+++ 30DaysOfCode/Day17/main.cpp
@@ -4,7 +4,17 @@
 #include <stdexcept>
 using namespace std;
 
-//Write your code here
+class Calculator
+{
+  public:
+    int power(int n, int p)
+    {
+      if(n < 0 || p < 0)
+        throw runtime_error("n and p should be non-negative");
+    
+      return pow(n, p);
+    }
+};
 
 int main()
 {
