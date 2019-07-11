e3dbf13119e8fcce08ea3030792f7125d914b17e
Ryan Carr <rycarr@gmail.com>
2019-07-11 14:03:34 -0500

    Basic Data Types - Solved
    Finished the code and matched all of the final criteria.

--- Cpp/003-Basic-Data-Types/main.cpp
+++ Cpp/003-Basic-Data-Types/main.cpp
@@ -2,7 +2,21 @@
 #include <cstdio>
 using namespace std;
 
-int main() {
-    // Complete the code.
+int main()
+{
+    int number;
+    long bigNumber;
+    char character;
+    float floatNumber;
+    double doubleNumber;
+
+    scanf("%d %ld %c %f %lf", &number, &bigNumber, &character, &floatNumber, &doubleNumber);
+
+    printf("%d\n", number);
+    printf("%ld\n", bigNumber);
+    printf("%c\n", character);
+    printf("%.3f\n", floatNumber);
+    printf("%.9g", doubleNumber);
+    
     return 0;
 }
