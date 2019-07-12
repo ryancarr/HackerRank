239cda5d61d03824c8f3c30e0e534774b88bd3cb
Ryan Carr <rycarr@gmail.com>
2019-07-12 12:38:26 -0500

    Day 19: Interfaces - Solved
    This is my solution to the problem posed by Day 19: Interfaces.

--- 30DaysOfCode/Day19/main.cpp
+++ 30DaysOfCode/Day19/main.cpp
@@ -11,8 +11,18 @@ class AdvancedArithmetic{
 };
 class Calculator : public AdvancedArithmetic {
 public:
-    int divisorSum(int n) {
-        return 0;
+    int divisorSum(int n)
+    {
+        int sum = n;
+        
+        for(int  i = 1; i <= n/2; i++)
+        {
+            if(n % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
     }
 };
 
