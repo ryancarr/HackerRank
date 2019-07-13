490da301247a6d945c10cc6cc8f50d945a23a65b
Ryan Carr <rycarr@gmail.com>
2019-07-13 11:22:11 -0500

    Day 20: Sorting - Solved
    This is the solution to the Bubble Sort challenge. Based heavily
    on their suggested algorithm in the instructions. Just added
    variables to keep track of certain stats as needed.

--- 30DaysOfCode/Day20/main.cpp
+++ 30DaysOfCode/Day20/main.cpp
@@ -11,24 +11,41 @@ int main()
     // Load values into vector
     for(int a_i = 0; a_i < n; a_i++)
     {
-    	cin >> a[a_i];
+        cin >> a[a_i];
     }
     
+    // We need to keep track of
+    // totalSwaps so we can let
+    // the user know how many
+    // swaps were performed
     int totalSwaps = 0;
 
+    // Sets the main logic back to the
+    // beginning of the array. Without
+    // this reset the values would only
+    // move one position to the right.
     for(int i = 0; i < n; i++)
     {
         int numberOfSwaps = 0;
 
+        // Main logic of the sort swaps
+        // any pair of values that are in
+        // descending order instead of
+        // ascending order
         for(int j = 0; j < n - 1; j++)
         {
             if(a[j] > a[j + 1])
             {
+                // Swap values
                 int temp = a[j + 1];
                 a[j + 1] = a[j];
                 a[j] = temp;
+
+                numberOfSwaps++;
             }
         }
+
+        totalSwaps += numberOfSwaps;
 
         if(numberOfSwaps == 0)
             break;
