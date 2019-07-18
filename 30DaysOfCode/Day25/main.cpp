63a666a34d07da6cb07c8bfe9db067db97402ce0
Ryan Carr <rycarr@gmail.com>
2019-07-18 17:55:54 -0500

    Day 25: Brute Force - Too slow

--- 30DaysOfCode/Day25/main.cpp
+++ 30DaysOfCode/Day25/main.cpp
@@ -5,8 +5,49 @@
 #include <algorithm>
 using namespace std;
 
+// Brute force prime number finder
+// VERY inefficient
+// I was hoping it might work for HackerRank
+bool isPrime(int number)
+{
+    if(number < 2 || (number % 2 == 0 && number != 2))
+        return false;
+    
+    for(int i = 3; i < number / 2 + 1; i++)
+    {
+        if(number % i == 0)
+        {
+            return false;
+        }
+    }
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return true;
+}
+
+int main()
+{
+    int quantity;
+    cin >> quantity;
+
+    vector<int> numbers;
+    numbers.reserve(quantity);
+
+    for(int i = 0; i < quantity; i++)
+    {
+        int number;
+        cin >> number;
+        numbers.push_back(number);
+    }
+
+    for(int number : numbers)
+    {
+        if(isPrime(number))
+            cout << "Prime" << endl;
+        else
+            cout << "Not prime" << endl;
+        
+    }
+
+
     return 0;
 }
