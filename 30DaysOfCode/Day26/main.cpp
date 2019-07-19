578e2940fe9c3a4bc63502e63ba385f01688a6bc
Ryan Carr <rycarr@gmail.com>
2019-07-19 11:54:23 -0500

    Day 26: Nested Logic - Solved
    This is the solution for the nested logic challenge.

--- 30DaysOfCode/Day26/main.cpp
+++ 30DaysOfCode/Day26/main.cpp
@@ -1,12 +1,28 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main()
+{
+    vector<int> actual(3);
+    vector<int> expected(3);
+
+    int fine;
+
+    cin >> actual[0] >> actual[1] >> actual[2];
+    cin >> expected[0] >> expected[1] >> expected[2];
+    
+    if(actual[2] > expected[2])
+        fine = 10000;
+    else if(actual[1] > expected[1] && actual[2] >= expected[2])
+        fine = 500 * (actual[1] - expected[1]);
+    else if(actual[0] > expected[0] && actual[1] >= expected[1] && actual[2] >= expected[2])
+        fine = 15 * (actual[0] - expected[0]);
+    else
+        fine = 0;
+
+    cout << fine;
+    
     return 0;
 }
