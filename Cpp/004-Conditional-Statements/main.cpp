a7d6a38c5ebffc602697e0f674fcde09e4e09676
Ryan Carr <rycarr@gmail.com>
2019-07-23 19:18:00 -0500

    Cpp - Conditional Statements - Solved
    This solution was accepted by the test and passed all of the test
    cases.

--- Cpp/004-Conditional-Statements/main.cpp
+++ Cpp/004-Conditional-Statements/main.cpp
@@ -9,7 +9,40 @@ int main()
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    // Write Your Code Here
+    // They ask for if else conditions I want to try a
+    // switch statement instead.
+    switch(n)
+    {
+        case 1:
+            cout << "one" << endl;
+            break;
+        case 2:
+            cout << "two" << endl;
+            break;
+        case 3:
+            cout << "three" << endl;
+            break;
+        case 4:
+            cout << "four" << endl;
+            break;
+        case 5:
+            cout << "five" << endl;
+            break;
+        case 6:
+            cout << "six" << endl;
+            break;
+        case 7:
+            cout << "seven" << endl;
+            break;
+        case 8:
+            cout << "eight" << endl;
+            break;
+        case 9:
+            cout << "nine" << endl;
+            break;
+        default:
+            cout << "Greater than 9" << endl;
+    };
 
     return 0;
 }
